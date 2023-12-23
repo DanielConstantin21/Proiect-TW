ebb5f0474ea9788f2d43bbfff5db087c32894aa0
Daniel Constantin <constantindaniel21@stud.ase.ro>
2023-12-23 13:01:36 +0200

    Artist Routes compl & Work routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const Artist = require("./database/models/Artist");
 const Work = require("./database/models/Work");
 const cors = require("cors");
 const artistRoutes = require("./routes/artistRoutes");
+const workRoutes = require("./routes/workRoutes");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,8 @@ app.get("/", function (req, res) {
   res.send("Hi");
 });
 Artist.hasMany(Work, { foreignKey: "artistId" });
+Work.belongsTo(Artist, { foreignKey: "artistId" });
 app.use("/artists", artistRoutes);
+app.use("/works", workRoutes);
 app.listen(8080);
 console.log("Server started on port 8080!");
--- server/routes/workRoutes.js
+++ server/routes/workRoutes.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const Artist = require("../database/models/Artist");
 const Work = require("../database/models/Work");
 const { Sequelize, Op } = require("sequelize");
+const Artist = require("../database/models/Artist");
 
 router.post("/", async (req, res) => {
   try {
+    const idArtist = req.body.artistId;
+    console.log(idArtist);
+    const artist = await Artist.findOne({ where: { id: idArtist } });
+    console.log(artist);
     if (!req.body || Object.keys(req.body).length === 0) {
       throw new Error("body is missing");
     }
 
-    const { id, title, birth_date } = req.body;
+    const { id, title, imageId } = req.body;
     if (
       !req.body.hasOwnProperty("id") ||
       !req.body.hasOwnProperty("title") ||
-      !req.body.hasOwnProperty("birth_date")
+      !req.body.hasOwnProperty("imageId") ||
+      !req.body.hasOwnProperty("artistId")
     ) {
       throw new Error("malformed request");
     }
@@ -22,19 +27,17 @@ router.post("/", async (req, res) => {
     if (isNaN(id) || id < 0) {
       throw new Error("id should be a positive number");
     }
-    const currentYear = new Date().getFullYear();
-    if (birth_date > currentYear - 5) {
-      throw new Error("invalid birth date");
-    }
-    const artist = await Artist.create({
+
+    const work = await Work.create({
       id,
       title,
-      birth_date,
+      imageId,
+      artistId: artist.id,
     });
     // Returnăm un răspuns corespunzător
     return res
       .status(201)
-      .json({ success: true, message: "artist created", data: artist }); // TODO
+      .json({ success: true, message: "artwork created", data: work }); // TODO
   } catch (err) {
     console.error(err.stack);
     console.log(err);
@@ -42,7 +45,7 @@ router.post("/", async (req, res) => {
     let errorMessage = "server error";
     if (err.name === "SequelizeUniqueConstraintError") {
       statusCode = 403;
-      errorMessage = "Artist already exists";
+      errorMessage = "Artwork already exists";
     }
     if (err.message === "body is missing") {
       statusCode = 400;
@@ -53,10 +56,6 @@ router.post("/", async (req, res) => {
     } else if (err.message === "id should be a positive number") {
       statusCode = 400;
       errorMessage = "id should be a positive number";
-    } else if (err.message === "invalid birth date") {
-      statusCode = 400;
-      errorMessage =
-        "Birth date is invalid. Artist should be at least 5 years old.";
     }
 
     res.status(statusCode).json({ message: errorMessage });
@@ -67,19 +66,19 @@ router.get("/search", async (req, res) => {
   try {
     const term = req.query.title;
     console.log(term);
-    const artists = await Artist.findAll({
+    const works = await Work.findAll({
       where: Sequelize.where(
         Sequelize.fn("LOWER", Sequelize.col("title")),
         "LIKE",
         `%${term.toLowerCase()}%`
       ),
     });
-    if (artists.length == 0) {
+    if (works.length == 0) {
       return res
         .status(404)
         .json({ success: false, message: "Not found", data: {} });
     }
-    res.status(200).json(artists);
+    res.status(200).json(works);
   } catch (err) {
     console.warn(err);
     res.status(500).json({ message: `Error occured: ${err}.` });
@@ -89,40 +88,61 @@ router.get("/search", async (req, res) => {
 router.get("/:id", async function (req, res) {
   try {
     const id = req.params.id;
+    const work = await Work.findByPk(id);
+    if (!work) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Artwork not found", data: {} });
+    }
+    return res.status(200).json(work);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error retrieving artwork.", data: {} });
+  }
+});
+
+//get works by artist id
+router.get("/artist/:id", async function (req, res) {
+  try {
+    const id = req.params.id;
     const artist = await Artist.findByPk(id);
     if (!artist) {
       return res
         .status(404)
         .json({ success: false, message: "Artist not found", data: {} });
     }
-    //const artistWorks = await artist.getWorks();
-    //return res.status(200).json({ artist, works: artistWorks });
-    return res.status(200).json(artist);
+    const works = await Work.findAll({ where: { artistId: artist.id } });
+    return res.status(200).json(works);
   } catch (err) {
     console.log(err);
     return res
       .status(500)
-      .json({ success: false, message: "Error retrieving artist.", data: {} });
+      .json({ success: false, message: "Error retrieving artwork.", data: {} });
   }
 });
 
+//get work by id
 router.put("/:id", async function (req, res) {
   try {
   } catch (err) {
     return res
       .status(500)
-      .json({ success: false, message: "Error retrieving artist.", data: {} });
+      .json({ success: false, message: "Error retrieving artwork.", data: {} });
   }
 });
 router.get("/", async function (req, res) {
   try {
-    const artists = await Artist.findAll();
-    res.status(200).json(artists);
+    const works = await Work.findAll();
+    res.status(200).json(works);
   } catch (err) {
     console.log(err);
-    return res
-      .status(500)
-      .json({ success: false, message: "Error retrieving artists.", data: {} });
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving artworks.",
+      data: {},
+    });
   }
 });
 module.exports = router;
